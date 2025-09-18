032c1522c44eee3019994d4716e6951c818f655b
calebgitau362-debug <calebgitau362@gmail.com>
2025-09-18 21:42:22 +0300

    Add files via upload

---
+++ 20250917-180042.c
@@ -0,0 +1,21 @@
+//c programming 
+/*
+Name:caleb Gitau
+Reg:CT100/G/26176/25
+Description:Units in all this semester
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Analogue electronics \n");
+    printf("Mathematic for science \n");
+    printf("Fundamentals of ict \n");
+    printf("introduction to programming & allogarithms\n");
+    printf("communication skills \n");
+    printf("Essentials of accounting \n");
+    printf("introduction to computer application \n");
+    printf("computer system & organisation \n");
+    return 0;
+    
+    }
