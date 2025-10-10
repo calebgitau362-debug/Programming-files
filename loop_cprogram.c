eef3c65315e2a826a9dffdc8ea3751b45c60afc8
calebgitau362-debug <calebgitau362@gmail.com>
2025-10-10 11:37:40 +0300

    Add files via upload

---
+++ loop_cprogram.c
@@ -0,0 +1,16 @@
+/*
+Name:caleb gitau
+Reg:CT/100/G/26176/25
+Description:for_loop c program
+*/
+
+#include<stdio.h>
+      
+  int main()  
+  {
+      int i;
+      for(i=100; i>50; i--){
+            printf("%d \n", i);
+      }      
+ return 0;
+   }      
