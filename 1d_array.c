795d5b5d32754c14dab753bab7a5d4da7a51c908
calebgitau362-debug <calebgitau362@gmail.com>
2025-10-30 22:04:17 +0300

    Add files via upload

---
+++ 1d_array.c
@@ -0,0 +1,31 @@
+/*
+NAME:caleb gitau
+REG NO:CT100/G/26176/25
+Description:1dimensional array
+*/
+#include <stdio.h>
+
+int main(){
+
+    float record[7];  
+    float sum = 0.0;
+    int i;
+
+    
+    char *days[] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"};
+
+    
+    for (i = 0; i < 7; i++)
+    {
+        printf("Enter %s record: ", days[i]);
+        scanf("%f", &record[i]);
+        sum += record[i]; 
+    }
+
+    
+    printf("\nTotal record for the week = %.2f\n", sum);
+
+    return 0;
+}
+
+    
